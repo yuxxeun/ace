9b3c9f3281e12388ebe71e3443d29fd05bf4e70e
yuxxeun <itsyuxxeun@gmail.com>
2024-03-12 13:06:26 +0700

    feat: location detector

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,10 +21,7 @@ export default function RootLayout({
       <html lang="en">
         <SmoothScrolling> 
           <body className={`${GeistSans.className} min-h-screen`}>
-          <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
-            <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-                  {children}
-            </div>
+            {children}
           </body>
         </SmoothScrolling>
       </html>
